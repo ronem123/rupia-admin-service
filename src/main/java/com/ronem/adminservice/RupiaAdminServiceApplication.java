9edce2802d37f6a1c70d4c34b62afaaa7c618f6a
Ram Mandal <ram.mandal@8squarei.com>
2026-03-27 09:52:43 +0545

    add filter, add secret to application.yaml

--- src/main/java/com/ronem/adminservice/RupiaAdminServiceApplication.java
+++ src/main/java/com/ronem/adminservice/RupiaAdminServiceApplication.java
@@ -1,13 +1,15 @@
 package com.ronem.adminservice;
 
+import com.ronem.rupiasecuritylib.config.SecurityLibConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(SecurityLibConfig.class)
 public class RupiaAdminServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RupiaAdminServiceApplication.class, args);
     }
